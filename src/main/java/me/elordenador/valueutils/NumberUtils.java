4cdebdab2f1859897e831cde774e7a5256c01f47
Daniel <danilacasito8@gmail.com>
2024-12-03 12:21:19 +0100

    Change random number generator for ValueUtils

--- src/main/java/me/elordenador/valueutils/NumberUtils.java
+++ src/main/java/me/elordenador/valueutils/NumberUtils.java
@@ -1,7 +1,7 @@
 package me.elordenador.valueutils;
 
 public class NumberUtils {
-    public void generateRandomNumberOfDigits(int numberOfDigits) {
+    public static void generateRandomNumberOfDigits(int numberOfDigits) {
         int number = 0;
         for (int i = 1; i <= numberOfDigits; i++) {
             number = number * 10 + (int) (Math.random() * 10);
